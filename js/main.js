f438346f6c3a9cad4e9444598fa9a3679209f1a2
Yaroslav Smirnov <yar14@mail.ru>
2020-02-09 15:18:45 +0300

    Feat (main.js): Первичная реализация 4-го ДЗ

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
 var offersMap = document.querySelector('.map');
+var filterForm = document.querySelector('form.map__filters');
+var offerForm = document.querySelector('form.ad-form');
+var addresInput = document.querySelector('#address');
+var roomNubmerSelect = document.querySelector('#room_number');
+var roomCapacitySelect = document.querySelector('#capacity');
+var mapPin = document.querySelector('.map__pin--main');
 
-var activateMap = function () {
-  offersMap.classList.remove('map--faded');
-};
+// Constants:
+var ENTER_KEYCODE = 'Enter';
+
+// flags:
+var isPinsRendered = false;
 
 var getRandomNumber = function (number) {
   return Math.floor(Math.random() * (number + 1));
@@ -40,6 +48,101 @@ var randomlyCutArray = function (array) {
   return shuffledItems.slice(getRandomNumber(shuffledItems.length));
 };
 
+var activateMap = function () {
+  offersMap.classList.remove('map--faded');
+  offerForm.classList.remove('ad-form--disabled');
+
+  if (!isPinsRendered) {
+    var rentOffers = generateRandomOffers(8);
+
+    renderPins(rentOffers);
+    isPinsRendered = true;
+  }
+};
+
+var setOfferAddres = function (offerPin, isActive) {
+  var MAP_PIN_WIDTH = mapPin.clientWidth;
+  var MAP_PIN_HEIGHT = mapPin.clientHeight;
+  var PIN_EDGE_HEIGTH = 22;
+  var x = offerPin.offsetLeft + (MAP_PIN_WIDTH / 2);
+  var y = offerPin.offsetTop;
+
+  if (isActive) {
+    y += MAP_PIN_HEIGHT + PIN_EDGE_HEIGTH;
+  } else {
+    y += MAP_PIN_HEIGHT / 2;
+  }
+
+  addresInput.value = Math.round(x) + ', ' + Math.round(y);
+};
+
+var disableFormInputs = function (form) {
+  var inputs = form.querySelectorAll('input');
+  var selects = form.querySelectorAll('select');
+
+  inputs.forEach(function (input) {
+    input.setAttribute('disabled', 'disabled');
+  });
+
+  selects.forEach(function (select) {
+    select.setAttribute('disabled', 'disabled');
+  });
+};
+
+var enableFormInputs = function (form) {
+  var inputs = form.querySelectorAll('input');
+  var selects = form.querySelectorAll('select');
+
+  inputs.forEach(function (input) {
+    input.removeAttribute('disabled');
+  });
+
+  selects.forEach(function (select) {
+    select.removeAttribute('disabled');
+  });
+};
+
+var validateCapacity = function () {
+  var options = roomCapacitySelect.querySelectorAll('option');
+  options.forEach(function (option) {
+    option.disabled = true;
+  });
+
+  switch (roomNubmerSelect.value) {
+    case '1':
+      roomCapacitySelect[0].disabled = false;
+      break;
+    case '2':
+      roomCapacitySelect[0].disabled = false;
+      roomCapacitySelect[1].disabled = false;
+      break;
+    case '3':
+      roomCapacitySelect[0].disabled = false;
+      roomCapacitySelect[1].disabled = false;
+      roomCapacitySelect[2].disabled = false;
+      break;
+    case '100':
+      roomCapacitySelect[0].disabled = false;
+      roomCapacitySelect[1].disabled = false;
+      roomCapacitySelect[2].disabled = false;
+      roomCapacitySelect[3].disabled = false;
+      break;
+  }
+};
+
+var checkRoomsCapasityValidity = function () {
+  if (roomCapacitySelect[roomCapacitySelect.selectedIndex].disabled) {
+    roomCapacitySelect.setCustomValidity('Неподходящее количество гостей');
+  } else {
+    roomCapacitySelect.setCustomValidity('');
+  }
+};
+
+var offerFormValidation = function () {
+  validateCapacity();
+  checkRoomsCapasityValidity();
+};
+
 var generateRandomOffers = function (quantity) {
   var RENT_TYPES = ['palace', 'flat', 'house', 'bungalo'];
   var CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
@@ -60,29 +163,31 @@ var generateRandomOffers = function (quantity) {
     var positionX = getRandomNumber(offersMap.clientWidth);
     var positionY = getRandomInteger(130, 630);
 
-    if (type === 'palace') {
-      title = 'Сдаю дворец!';
-      rooms = getRandomInteger(10, 20);
-      price = 100 * rooms + features.length * 20;
-      description = '+100500 к авторитету';
-    }
-    if (type === 'flat') {
-      title = 'Сдаю квартиру';
-      rooms = getRandomInteger(1, 4);
-      price = 25 * rooms + features.length * 3;
-      description = 'Невероятные виды из окна';
-    }
-    if (type === 'house') {
-      title = 'Сдаю дом';
-      rooms = getRandomInteger(2, 6);
-      price = 35 * rooms + features.length * 5;
-      description = 'Енот в придачу, бесплатно!';
-    }
-    if (type === 'bungalo') {
-      title = 'Сдаю хату';
-      rooms = getRandomInteger(1, 2);
-      price = 10 * rooms + features.length * 2;
-      description = 'Предыдущего владельца съели утки';
+    switch (type) {
+      case 'palace':
+        title = 'Сдаю дворец!';
+        rooms = getRandomInteger(10, 20);
+        price = 100 * rooms + features.length * 20;
+        description = '+100500 к авторитету';
+        break;
+      case 'flat':
+        title = 'Сдаю квартиру';
+        rooms = getRandomInteger(1, 4);
+        price = 25 * rooms + features.length * 3;
+        description = 'Невероятные виды из окна';
+        break;
+      case 'house':
+        title = 'Сдаю дом';
+        rooms = getRandomInteger(2, 6);
+        price = 35 * rooms + features.length * 5;
+        description = 'Енот в придачу, бесплатно!';
+        break;
+      case 'bungalo':
+        title = 'Сдаю хату';
+        rooms = getRandomInteger(1, 2);
+        price = 10 * rooms + features.length * 2;
+        description = 'Предыдущего владельца съели утки';
+        break;
     }
 
     var randomOffer = {
@@ -138,7 +243,41 @@ var renderPins = function (pins) {
   offersMap.appendChild(fragment);
 };
 
+mapPin.addEventListener('mousedown', function (evt) {
+  if (evt.button === 0) {
+    activateMap();
+    enableFormInputs(offerForm);
+    enableFormInputs(filterForm);
+    setOfferAddres(mapPin, true);
+  }
+});
 
-var rentOffers = generateRandomOffers(8);
-renderPins(rentOffers);
-activateMap();
+mapPin.addEventListener('keydown', function (evt) {
+  if (evt.code === ENTER_KEYCODE) {
+    activateMap();
+    enableFormInputs(offerForm);
+    enableFormInputs(filterForm);
+    setOfferAddres(mapPin, true);
+  }
+});
+
+roomNubmerSelect.addEventListener('input', function () {
+  validateCapacity();
+});
+
+roomCapacitySelect.addEventListener('change', function () {
+  checkRoomsCapasityValidity();
+});
+
+offerForm.addEventListener('submit', function () {
+  checkRoomsCapasityValidity();
+});
+
+var init = function () {
+  disableFormInputs(offerForm);
+  disableFormInputs(filterForm);
+  offerFormValidation();
+  setOfferAddres(mapPin, false);
+};
+
+init();
